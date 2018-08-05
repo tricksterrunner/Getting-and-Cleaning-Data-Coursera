bd08f67f5fcb38bcdedcfe97c7039fe0903924de
tricksterrunner <38614901+tricksterrunner@users.noreply.github.com>
2018-08-06 01:36:53 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,12 @@
 library(reshape2)
 
-
-
 filename <- "getdata_dataset.zip"
-
-
 
 ## Download and unzip the dataset:
 
 if (!file.exists(filename)){
-
   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
-
   download.file(fileURL, filename, method="curl")
-
 }  
 
 if (!file.exists("UCI HAR Dataset")) { 
